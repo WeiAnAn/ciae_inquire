1bae2be749580acf98619ca7e25b6b2a825ce31e
WeiAnAn <j22461615@gmail.com>
2016-11-15 15:49:03 +0800

    add home view update font-awesome

---
+++ resources/views/layouts/side_menu.blade.php
@@ -0,0 +1,73 @@
+<div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        
+                        <li>
+                            <a href="#"><i class="fa fa-address-card-o fa-fw" aria-hidden="true"></i> 基本資料<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="flot.html">單位資料修改</a>
+                                </li>
+                                <li>
+                                    <a href="morris.html">英檢畢業門檻</a>
+                                </li>
+                                <li>
+                                    <a href="morris.html">全外語授課之課程</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-briefcase fa-fw" aria-hidden="true"></i> 教師、研究員專區<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="1">本校教師赴國外出席國際會議</a>
+                                </li>
+                                <li>
+                                    <a href="1">本校教師赴國外交換</a>
+                                </li>
+                                <li>
+                                    <a href="1">本校教師赴國外研究</a>
+                                </li>
+                                <li>
+                                    <a href="1">外籍學者蒞校訪問</a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <a href="#"><i class="fa fa-graduation-cap fa-fw"></i> 學生專區<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li><a href="1">赴國外出席國際會議</a></li>
+                                <li><a href="1">出國赴姊妹校參加交換計畫</a></li>
+                                <li><a href="1">其他出國研修情形</a></li>
+                                <li><a href="1">姊妹校學生至本校參加交換計畫</a></li>
+                                <li><a href="1">外籍學生至本校短期交流訪問</a></li>
+                                <li><a href="1">修讀正式學位之外國學生</a></li>
+                            </ul>
+                        </li>
+                            <!-- /.nav-second-level -->
+
+                        <li>
+                            <a href="#"><i class="fa fa-wrench fa-fw"></i> 其他國際交流活動<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="panels-wells.html">跨國學位</a>
+                                </li>
+                                <li>
+                                    <a href="buttons.html">姊妹校締約情形</a>
+                                </li>
+                                <li>
+                                    <a href="notifications.html">國際合作交流計畫</a>
+                                </li>
+                                <li>
+                                    <a href="typography.html">國際化活動</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+            <!-- /.navbar-static-side -->
--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,60 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'Auth\LoginController@showLoginForm');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index');
-Route::get('/index', function(){
-	return view('index');
+Route::group(['middleware' => 'auth'],function(){
+	Route::get('/home',['as' => 'home', 'uses' => 'HomeController@index']);
+	
+	Route::get('/user', function(){
+		return view('user');
+	});
+	Route::get('/graduate_threshold',function(){
+		return view('graduate_threshold');
+	});
+	Route::get('/foreign_language_class',function(){
+		return view('foreign_language_class');
+	});
+	Route::get('/cooperation_proj',function(){
+		return view('cooperation_proj');
+	});
+	Route::get('/foreign_prof_vist',function(){
+		return view('foreign_prof_vist');
+	});
+	Route::get('/foreign_stu',function(){
+		return view('foreign_stu');
+	});
+	Route::get('/partner_school',function(){
+		return view('partner_school');
+	});
+	Route::get('/prof_attend_conference',function(){
+		return view('prof_attend_conference');
+	});
+	Route::get('/prof_exchange',function(){
+		return view('prof_exchange');
+	});
+	Route::get('/prof_foreign_research',function(){
+		return view('prof_foreign_research');
+	});
+	Route::get('/short_term_foreign_stu',function(){
+		return view('short_term_foreign_stu');
+	});
+	Route::get('/stu_attend_conf',function(){
+		return view('stu_attend_conf');
+	});
+	Route::get('/stu_foreign_research',function(){
+		return view('stu_foreign_research');
+	});
+	Route::get('/stu_from_partner_school',function(){
+		return view('stu_from_partner_school');
+	});
+	Route::get('/stu_to_partner_school',function(){
+		return view('stu_to_partner_school');
+	});
+	Route::get('/transnational_degree',function(){
+		return view('transnational_degree');
+	});
+
 });
