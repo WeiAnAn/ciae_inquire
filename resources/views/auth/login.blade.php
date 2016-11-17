514110409b6e2212953d25aa7e0fe07e9536e4c1
WeiAnAn <j22461615@gmail.com>
2016-11-18 01:40:11 +0800

    update login view to chinese

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,25 +39,25 @@
             <div class="col-md-4 col-md-offset-4">
                 <div class="login-panel panel panel-default">
                     <div class="panel-heading">
-                        <h3 class="panel-title">Please Sign In</h3>
+                        <h3 class="panel-title">國際化調查系統</h3>
                     </div>
                     <div class="panel-body">
                         <form role="form" method="post" action="{{ url('/login') }}">
                          {{ csrf_field() }}
                             <fieldset>
                                 <div class="form-group">
-                                    <input class="form-control" placeholder="Username" name="username" type="text" autofocus>
+                                    <input class="form-control" placeholder="帳號" name="username" type="text" autofocus>
                                 </div>
                                 <div class="form-group">
-                                    <input class="form-control" placeholder="Password" name="password" type="password" value="">
+                                    <input class="form-control" placeholder="密碼" name="password" type="password" value="">
                                 </div>
-                                <div class="checkbox">
+                                <!-- <div class="checkbox">
                                     <label>
-                                        <input name="remember" type="checkbox" value="Remember Me">Remember Me
+                                        <input name="remember" type="checkbox" value="Remember Me">記住我
                                     </label>
-                                </div>
+                                </div> -->
                                 <!-- Change this to a button or input when using this as a form -->
-                                <button class="btn btn-lg btn-success btn-block">Login</button>
+                                <button class="btn btn-lg btn-success btn-block">登入</button>
                             </fieldset>
                         </form>
                     </div>
