4cbee5c74ec08f1b489781e1e37d8a1702f9f502
WeiAnAn <j22461615@gmail.com>
2016-11-19 03:06:03 +0800

    add password input wrong alert remove nav setting icon

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -21,9 +21,7 @@
 
                     </a>
                     <ul class="dropdown-menu dropdown-user">
-                        <li><a href="#"><i class="fa fa-user fa-fw"></i> 個人資料</a>
-                        </li>
-                        <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+                        <li><a href="{{url('/user')}}"><i class="fa fa-user fa-fw"></i> 個人資料</a>
                         </li>
 
                         <li class="divider"></li>
