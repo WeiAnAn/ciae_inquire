500ccf1bfcf872355e4f4f1b8b3e2e9339d42bf7
wangyaoching <wang7186wang@gmail.com>
2016-12-14 17:15:26 +0800

    new controller

---
+++ app/Http/Controllers/prof/ForeignProfExchangeController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\prof;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ForeignProfExchangeController extends Controller
+{
+    //
+}
