67d6b194a2b6c58af79252109d293ecc4212524e
wlunareve <wlunareve@gmail.com>
2016-12-15 00:33:14 +0800

    add view of international_journal_editor

--- resources/views/other/international_journal_editor.blade.php
+++ resources/views/other/international_journal_editor.blade.php
@@ -27,6 +27,161 @@
                 <li><a href="#upload" data-toggle="tab">批次上傳</a>
                 </li>
             </ul>
+            	<div class="tab-content">
+					@if(count($errors)>0)
+						<div class="tab-pane fade in table-responsive" id="show" 
+							style="margin-top: 10px">
+					@else
+						<div class="tab-pane fade in active table-responsive" id="show" 
+							style="margin-top: 10px">
+					@endif
+						@if(session('success'))
+				        <div class="alert alert-success alert-dismissible" role="alert">
+				            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				                <span aria-hidden="true">&times;</span>
+				            </button>
+				            <strong> {{ session('success') }}</strong>
+				        </div>
+			        	@endif
+						<table width="100%" class="table table-striped table-bordered table-hover">
+							<thead>	
+								<tr>
+									<td id="international_journal_editor.college" onclick="sort(id)">所屬一級單位</td>
+									<td id="international_journal_editor.dept" onclick="sort(id)">所屬系所部門</td>
+									<td id="name" onclick="sort(id)">期刊編輯者</td>
+									<td id="" onclick="sort(id)">期刊名稱</td>
+									<td id="startDate" onclick="sort(id)">開始擔任時間</td>
+									<td id="endDate" onclick="sort(id)">結束擔任時間</td>
+									<td id="comments" onclick="sort(id)">備註</td>
+									<td>管理</td>
+								</tr>
+							</thead>
+							<tbody>
+							</tbody>
+						</table>
+					</div>
+
+					<!--insert page-->
+
+					@if(count($errors)>0)
+						<div class="tab-pane fade in col-md-12 active " id="insert"
+							style="margin-top: 10px">
+					@else
+						<div class="tab-pane fade in col-md-12 " id="insert" 
+							style="margin-top: 10px">
+					@endif
+						<form action="{{url('international_journal_editor')}}" method="post">
+							{{ csrf_field() }}
+							@include('../layouts/select')
+
+							@if($errors->has('name'))
+                                <p class="text-danger">{{$errors->first('name')}}</p>
+                            @endif
+							<div class="form-group">
+								<label for="">期刊編輯者姓名</label>
+								<input type="text" class="form-control" name="name" value="{{old('name')}}">
+							</div>
+
+							{{-- 這裡需要改NAME --}}
+
+							@if($errors->has('nation'))
+                                <p class="text-danger">{{$errors->first('nation')}}</p>
+                            @endif
+							<div class="form-group">
+								<label for="nation">期刊名稱</label>
+								<input type="text" name="nation" class="form-control" value="{{old('nation')}}">
+							</div>
+							
+							@if($errors->has('startDate')||$errors->has('endDate'))
+                                <p class="text-danger col-md-6">{{ $errors->first('startDate')}}</p>                      
+                                <p class="text-danger col-md-6">{{ $errors->first('endDate')}}</p>
+                            @endif
+							<div class="form-group col-md-6" style="padding-left:0 ;padding-right: 0">
+								<label for="startDate">開始擔任時間</label>
+								<input type="date" name="startDate" class="form-control" value="{{old('startDate')}}">
+							</div>
+							<div class="form-group col-md-6" style="padding-left:0 ;padding-right: 0">
+								<label for="endDate">結束擔任時間</label>
+								<input type="date" name="endDate" class="form-control" value="{{old('endDate')}}">
+							</div>
+
+							@if($errors->has('comments'))
+                                <p class="text-danger">{{$errors->first('comments')}}</p>
+                            @endif
+							<div class="form-group">
+								<label for="comments">備註</label>
+								<textarea name="comments" id="comments" cols="30" rows="3" class="form-control">{{old('comments')}}</textarea>
+							</div>
+							<button class="btn btn-success">新增</button>
+						</form>
+					</div>
+
+					<div class="tab-pane fade in col-md-12" id="search" style="margin-top: 10px;">
+						<div class="alert alert-success alert-dismissible" role="alert">
+				            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				                <span aria-hidden="true">&times;</span>
+				            </button>
+				            <strong>不加入搜尋條件之選項留空即可</strong>
+				        </div>
+						<form action="{{url('international_journal_editor/search')}}">
+							@include('../layouts/select_search')
+							<div class="form-group">
+								<label for="">期刊編輯者姓名</label>
+								<input type="text" class="form-control" name="name" />
+							</div>
+							
+							<div class="form-group">
+								<label for="nation">期刊名稱</label>
+								<input type="text" name="nation" class="form-control">
+							</div>
+							<div class="form-group" style="margin-bottom: 0px">
+								<label >開始擔任日期</label>
+							</div>
+							<div class="form-group col-md-6" style="padding-left:0 ;padding-right: 0">
+								<label for="startDate">從</label>
+								<input type="date" name="startDate" class="form-control">
+							</div>
+							<div class="form-group col-md-6" style="padding-left:0 ;padding-right: 0">
+								<label for="endDate">到</label>
+								<input type="date" name="endDate" class="form-control">
+							</div>
+							<div class="form-group">
+								<label for="comments">備註</label>
+								<textarea name="comments" id="comments" cols="30" rows="3" class="form-control"></textarea>
+							</div>
+							<button class="btn btn-success">搜尋</button>
+						</form>
+					</div>
+
+					<div class="tab-pane fade in col-md-12" id="upload" style="margin-top: 10px;">
+						<form action="{{url('international_journal_editor/upload')}}" method="post" enctype="multipart/form-data">
+							{{ csrf_field() }}
+                        	<div id="file_error"></div>
+                        	@if(count($errors->upload)>0)
+                        		@if($errors->upload->has('format'))
+                        			<p class="text-danger">
+										{{$errors->upload->first('format')}}
+                        			</p>
+                        		@elseif($errors->upload->has('permission'))
+                        			<p class="text-danger">
+                        				{{$errors->upload->first('permission')}}
+                        			</p>
+                        		@elseif($errors->upload->has('number'))
+                        			<p class="text-danger">
+                        				{{$errors->upload->first('number')}}
+                        			</p>
+                        		@else
+                        			<p class="text-danger">
+                        				欄位內容格式錯誤或必填欄位未填
+                        			</p>
+                        		@endif
+                        	@endif
+							<input type="file" name="file" class="" style="margin: 2px">
+							<button class="btn btn-primary" style="margin: 2px">上傳</button>
+							<a class="btn btn-success" href="{{url('international_journal_editor/example')}}">範例檔案</a>
+							<a class="btn btn-success" href="{{url('example')}}">系所對照表</a>									
+						</form>
+					</div>
 
             
             </div>
