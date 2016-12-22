b4d79dd4e400d0a3941d77c7cf826bea335fab28
WeiAnAn <j22461615@gmail.com>
2016-12-22 19:24:49 +0800

    update stu_from-partner_school

--- resources/views/stu/stu_from_partner_school.blade.php
+++ resources/views/stu/stu_from_partner_school.blade.php
@@ -6,7 +6,9 @@
 
 <div class="row">
 	<div class="col-md-12">
-		<h1 class="page-header">姊妹校學生至本校參加交換計畫</h1>
+		<a href="{{URL('stu_from_partner_school')}}" style="color: black">
+			<h1 class="page-header">姊妹校學生至本校參加交換計畫</h1>
+		</a>
 	</div>
 </div>
 <div class="row">	
@@ -49,15 +51,23 @@
 						<table width="100%" class="table table-striped table-bordered table-hover">
 							<thead>	
 								<tr>
-									<td id="stu_from_partner_school.college" onclick="sort(id)">所屬一級單位</td>
-									<td id="stu_from_partner_school.dept" onclick="sort(id)">所屬系所部門</td>
-									<td id="name" onclick="sort(id)">姓名</td>
-									<td id="stuLevel" onclick="sort(id)">身分 (學士、碩士或博士班）</td>
-									<td id="nation" onclick="sort(id)">國籍</td>
-									<td id="startDate" onclick="sort(id)">開始時間</td>
-									<td id="endDate" onclick="sort(id)">結束時間</td>
-									<td id="comments" onclick="sort(id)">備註</td>
-									<td>管理</td>
+									<td id="stu_from_partner_school.college" class="text-nowrap" 
+										onclick="sort(id)">所屬單位</td>
+									<td id="stu_from_partner_school.dept" class="text-nowrap" 
+										onclick="sort(id)">系所部門</td>
+									<td id="name" class="text-nowrap" 
+										onclick="sort(id)">姓名</td>
+									<td id="stuLevel" class="text-nowrap" 
+										onclick="sort(id)">身分</td>
+									<td id="nation" class="text-nowrap" 
+										onclick="sort(id)">國籍</td>
+									<td id="startDate" class="text-nowrap" 
+										onclick="sort(id)">開始時間</td>
+									<td id="endDate" class="text-nowrap" 
+										onclick="sort(id)">結束時間</td>
+									<td id="comments" class="text-nowrap" 
+										onclick="sort(id)">備註</td>
+									<td class="text-nowrap">管理</td>
 								</tr>
 								</thead>
 								<tbody>
@@ -76,8 +86,8 @@
 									@endif
 									</td>
 									<td>{{$data->nation}}</td>
-									<td>{{$data->startDate}}</td>
-									<td>{{$data->endDate}}</td>
+									<td class="text-nowrap">{{$data->startDate}}</td>
+									<td class="text-nowrap">{{$data->endDate}}</td>
 									<td>{{$data->comments}}</td>
 									<td class="text-nowrap">
 										@can('permission',$data)
